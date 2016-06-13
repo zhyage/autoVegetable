f4859e1080708d22c3924c04b3a29b89924a0cad
zhyage <yong.zhy@gmail.com>
2016-06-13 22:16:26 +0800

    dhcpd done
    dhcpd done

--- actionBoard/clientUno2/ethernetHandle/ethernetHandle.cpp
+++ actionBoard/clientUno2/ethernetHandle/ethernetHandle.cpp
@@ -61,7 +61,7 @@ size_t ethernetHandle::UDPrecvPacket(uint8_t *recvMsg)
 
 bool ethernetHandle::UDPSendPacket(uint8_t *msg, size_t length)
 {
-    IPAddress rip(192, 168, 199, 151);
+    IPAddress rip(10, 0, 0, 1);
     Udp.beginPacket(rip, 8877);
     Udp.write(msg, length);
     Udp.endPacket();
